0c0fa93e66b23954916f5a4f02b2b91d32c99bca
Hemanth Veeramalli <167007806+HemanthVeeramalli@users.noreply.github.com>
2024-04-14 19:44:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function welcome(){
+    window.open("https://www.facebook.com/prince.hemanth.NTR/")
+}
+
+function welcome1(){
+    window.open("https://www.instagram.com/i_prince_hemanth/")
+}
+function welcome2(){
+    window.open("https://twitter.com/IPrincehemanth")
+}
+function welcome3(){
+    window.open("https://www.linkedin.com/in/hemanth-v-509355214/")
+}
+function welcome4(){
+    window.open("https://www.youtube.com/@thequestmedia7207")
+}
+function welcome5(){
+    window.open("https://github.com/Hemanth3096?tab=repositories")
+}
+
+
+scrollContainer.addEventListener('mouseenter', () => {
+    // Pause the scrolling animation when hovering over the images
+    scrollContainer.style.animationPlayState = 'paused';
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+    // Resume the scrolling animation when the mouse leaves the images
+    scrollContainer.style.animationPlayState = 'running';
+});
+
+_
